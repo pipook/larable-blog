fd81c32feab402f0f785150e9db8a7717f50cb0c
Daniel Chico <pipook@gmail.com>
2022-05-23 17:15:16 -0500

    Section 8 'Laughably Simple Pagination, part 1'

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,7 @@
     <main class="max-w-6xl mx-auto mt-20 space-y-6">
         @if($posts->count())
             <x-posts-grid :posts="$posts" />
+            {{ $posts->links() }}
         @else
             <p>No posts yet. Please check back later.</p>
         @endif
