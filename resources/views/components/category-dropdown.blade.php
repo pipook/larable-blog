f96e22aac23bb0dc2041339f08a7296f37f51844
Daniel Chico <pipook@gmail.com>
2022-05-24 16:37:52 -0500

    Section 8 'Laughably Simple Pagination, part 2'

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -5,14 +5,13 @@
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
         </button>
     </x-slot>
-    <x-dropdown-item 
-        href="/?{{ http_build_query(request()->except('category', 'page')) }}" 
+    <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
         :active="request()->routeIs('home')"
-    >
-        All
+    >All
     </x-dropdown-item>
     @foreach($categories as $category)
-        <x-dropdown-item 
+        <x-dropdown-item
             href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active="request()->is('categories/' . $category->slug)"
         >{{ ucwords($category->name) }}</x-dropdown-item>
