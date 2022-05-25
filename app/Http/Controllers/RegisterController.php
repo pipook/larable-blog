095dd55fae5fb3af353cce25caa37b6aa1169134
Daniel Chico <pipook@gmail.com>
2022-05-25 15:14:32 -0500

    Section 9 'Failed Validation and Old Input Data'

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -16,8 +17,8 @@ class RegisterController extends Controller
         //return request()->all();
         $attributes = request()->validate([
             'name'=> ['required', 'max:255'],
-            'username'=> ['required', 'min:3', 'max:255'],
-            'email'=> ['required', 'email', 'max:255'],
+            'username'=> ['required', 'min:3', 'max:255', Rule::unique('users', 'username')/*'unique:users,username'*/],
+            'email'=> ['required', 'email', 'max:255', Rule::unique('users', 'email')],
             'password'=> ['required', 'min:7', 'max:18']
         ]);
         //$attributes['password'] = bcrypt($attributes['password']);
