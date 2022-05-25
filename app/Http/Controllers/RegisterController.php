cc4e47d2e377d6213702b69ccbab5fd9099faac4
Daniel Chico <pipook@gmail.com>
2022-05-25 14:49:15 -0500

    Section 9 'Automatic Password Hashing With Mutators'

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,6 +20,7 @@ class RegisterController extends Controller
             'email'=> ['required', 'email', 'max:255'],
             'password'=> ['required', 'min:7', 'max:18']
         ]);
+        //$attributes['password'] = bcrypt($attributes['password']);
         User::create($attributes);
         return redirect('/');
     }
