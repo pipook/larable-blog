13d3078dd8a46c48ab3c212c99a35da6769e6a7c
Daniel Chico <pipook@gmail.com>
2022-05-26 14:19:45 -0500

    Section 10 'Write the Markup for a Post Comment'

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,10 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0"><img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl"></div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted <time>8 months ago</time></p>
+        </header>
+        <p>Mollit nostrud deserunt est voluptate. Exercitation id dolore ut eiusmod ullamco nostrud id. Deserunt amet nulla cupidatat ad deserunt eiusmod ipsum excepteur laborum veniam. Laboris tempor consectetur amet tempor ipsum eu ullamco magna in velit.</p>
+    </div>
+</article>
