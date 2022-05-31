54149ca33a50413f090763dc9f897a1ec07e198e
Daniel Chico <pipook@gmail.com>
2022-05-31 17:04:50 -0500

    Section 10 'Some Light Chapter Clean Up'

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -57,7 +57,10 @@
                                     <h2 class="ml-4">Want to participate?</h2>
                                 </header>
                                 <section class="mt-6">
-                                    <textarea name="body" class="w-full text-sm focus:outline-none focus:ring p-2" rows="5" placeholder="Quick, thing of smoething to say!"></textarea>
+                                    <textarea name="body" class="w-full text-sm focus:outline-none focus:ring p-2" rows="5" placeholder="Quick, thing of smoething to say!" required></textarea>
+                                    @error('body')
+                                        <span class="text-xs text-red-500">{{ $message }}</span>
+                                    @enderror
                                 </section>
                                 <footer class="flex justify-end mt-6 pt-6 border-t border-gray-200">
                                     <button type="submit" class="bg-blue-500 text-white font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600 uppercase">Post</button>
